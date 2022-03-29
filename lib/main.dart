0363d5fb2186cdab001a2141611736cb575418df
tgeery <tpgeery@socal.rr.com>
2022-03-29 00:54:30 -0700

    finsh first two games

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class WhereType {
   String name;
@@ -11,6 +12,7 @@ class MascotType {
   String img_path;
   String school;
   String name;
+  MascotType(this.img_path, this.school, this.name);
 }
 
 List<WhereType> where_list = [
@@ -47,32 +49,35 @@ List<WhereType> where_list = [
   WhereType("Troy", "Troy", "Alabama"),
   WhereType("Tufts", "Medford & Somerville", "Massachusetts"),
   WhereType("Vanderbilt", "Nashville", "Tennessee"),
-  WhereType("Villanova", "Phiadelphia", "Pennsylvania"),
+  WhereType("Villanova", "Philadelphia", "Pennsylvania"),
   WhereType("Wake Forest", "Winston-Salem", "North Carolina"),
   WhereType("Xavier", "Cincinnati", "Ohio")
 ];
 
-const List<String> imgs = [
-  'Bakersfield_Roadrunners_logo.svg.png',
-  'Channel_Islands_Dolphins.png',
-  'Chico_State_Wildcats_logo.svg.png',
-  'Dominguez_Hills_Toros.png',
-
-  'San_Diego_State_Aztecs_logo.svg.png',
-  'Fullerton_Titans.png',
-  'Long_Beach_State_49ers.png',
-  
-];
-
-const List<MascotType> schools = [
-  MascotType('BAKERSFIELD - ROADRUNNERS', 'Rowdy'), // the Roadrunner
-  MascotType('CHANNEL ISLANDS - DOLPHINS', 'Ekho'), // the Dolphin
-  MascotType('CHICO STATE - WILDCATS', 'Wildcats'),
-  MascotType('DOMINGUEZ HILLS - TOROS', 'Toros'),
-
-  MascotType('SAN DIEGO STATE - AZTECS', 'Aztec Warrior'),
-  MascotType('FULLERTON - TITANS', 'Tuffy the Titan (Elephant)'),
-  MascotType('LONG BEACH STATE - 49ERS', 'Elby the Shark'),
+List<MascotType> mascot_list = [
+  MascotType('../images/Bakersfield_Roadrunners_logo.svg.png', 'BAKERSFIELD - ROADRUNNERS', 'Rowdy'), // the Roadrunner
+  MascotType('../images/Channel_Islands_Dolphins.png', 'CHANNEL ISLANDS - DOLPHINS', 'Ekho'), // the Dolphin
+  MascotType('../images/Chico_State_Wildcats_logo.svg.png', 'CHICO STATE - WILDCATS', 'Willie'),
+  MascotType('../images/Dominguez_Hills_Toros.png', 'DOMINGUEZ HILLS - TOROS', 'Teddy'),
+  MascotType('../images/East_Bay_Pioneers.jpeg', 'EAST BAY CSU - PIONEERS', 'Pete'),
+  MascotType('../images/Fresno_State_Bulldogs.png', 'FRESNO STATE - BULLDOGS', 'Timeout'),
+  MascotType('../images/Fullerton_Titans.jpeg', 'FULLERTON - TITANS', 'Tuffy'),
+  MascotType('../images/Humbolt_Lumberjacks.jpeg', 'HUMBOLDT POLYTECHNIC - LUMBERJACKS', 'Lucky'),
+  MascotType('../images/Long_Beach_49ers.jpeg', 'LONG BEACH STATE - 49ERS', 'Elee'), // the Shark
+  MascotType('../images/Los_Angeles_Golden_Eagles_logo.svg.png', 'LOS ANGELES STATE - GOLDEN EAGLES', 'Eddie'),
+  // MascotType('', 'MARITIME ACADEMY - GOLDEN BEARS', '')
+  MascotType('../images/Monterey_Bay_Otters.png', 'MONTEREY BAY - OTTERS', 'Rey'),
+  MascotType('../images/Northridge_Matadors.png', 'NORTHRIDGE - MATADORS', 'Matty'),
+  MascotType('../images/Pomona_Broncos_logo.svg.png', 'POMONA POLYTECHNIC - BRONCOS', 'Billy'),
+  MascotType('../images/Sacramento_Hornets.jpeg', 'SACREMENTO - HORNETS', 'Herky'),
+  MascotType('../images/San_Bernardino_Coyotes_logo.svg.png', 'SAN BERNARDINO - COYOTES', 'Cody'),
+  MascotType('../images/San_Diego_St_Aztecs.png', 'SAN DIEGO STATE - AZTECS', 'Aztec Warrior'),
+  MascotType('../images/San_Francisco_Gators.png', 'SAN FRANCISCO STATE - GATORS', ''),
+  MascotType('../images/San_Jose_State_Spartans_logo.svg.png', 'SAN JOSE STATE - SPARTANS', 'Sammy'),
+  MascotType('../images/San_Luis_Obispo_Mustangs.png', 'SAN LUIS OBISPO POLYTECHNIC - MUSTANGS', 'Musty'),
+  MascotType('../images/San_Marcos_Cougars.jpeg', 'SAN MARCOS - Cougars', 'Crash'),
+  MascotType('../images/Sonoma_Seawolves.png', 'SONOMA - Seawolves', 'Lobo'),
+  MascotType('../images/Stanislaus_Warriors.png', 'STANISLAUS - WARRIORS', 'Titus')
 ];
 
 void main() {
@@ -95,13 +100,29 @@ class MyApp extends StatelessWidget {
 }
 
 class AutocompleteText extends StatelessWidget {
-  const AutocompleteText(this.lst);
+  AutocompleteText(this.lst, this.init, this.setController, this.enabled);
 
   final List<String> lst;
+  final String init;
+  final bool enabled;
+  var setController;
 
   @override
   Widget build(BuildContext context) {
     return Autocomplete<String>(
+      fieldViewBuilder: (context, controller, focusNode, onEditingComplete) {
+        setController(controller);
+        return TextField(
+          controller: controller,
+          enabled: enabled,
+          focusNode: focusNode,
+          onEditingComplete: onEditingComplete,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: init,
+          ),
+        );
+      },
       optionsBuilder: (TextEditingValue textEditingValue) {
         if(textEditingValue.text == '') {
           return const Iterable<String>.empty();
@@ -117,7 +138,31 @@ class AutocompleteText extends StatelessWidget {
   }
 }
 
-class LocationGame extends StatelessWidget {
+class LocationGame extends StatefulWidget {
+  LocationGame({Key? key, this.idx = 0, this.reset}) : super(key: key);
+
+  int idx;
+  var reset;
+  TextEditingController cityController = TextEditingController();
+  TextEditingController stateController = TextEditingController();
+  String correctCity = "";
+  String correctState = "";
+  String guessedCity = "";
+  String guessedState = "";
+
+  @override
+  State<LocationGame> createState() => LocationGameState();
+}
+
+class LocationGameState extends State<LocationGame> {
+  void setCityController(cntrl) {
+    widget.cityController = cntrl;
+  }
+
+  void setStateController(cntrl) {
+    widget.stateController = cntrl;
+  }
+
   @override
   Widget build(BuildContext context) {
     List<String> cities = [], states = [];
@@ -125,47 +170,229 @@ class LocationGame extends StatelessWidget {
       cities.add(college.city);
       states.add(college.state);
     });
+
+    widget.correctCity = where_list[widget.idx].city;
+    widget.correctState = where_list[widget.idx].state;
+
+    var footer = Column(
+      children: <Widget>[
+        TextButton(
+          style: ButtonStyle(
+            foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+            backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+          ),
+          onPressed: () {
+            setState(() {
+              widget.guessedCity = widget.cityController.text;
+              widget.guessedState = widget.stateController.text;
+              print(widget.guessedCity);
+            });
+          },
+          child: Text('Submit'),
+        ),
+      ]
+    );
+
+    bool ena = true;
+    int len = widget.guessedCity.length;
+    print('length $len');
+    if(widget.guessedCity.length > 0) {
+      ena = false;
+      var result = "Incorrect";
+      var other = "";
+      if(widget.guessedCity == widget.correctCity && widget.guessedState == widget.correctState) {
+        var result = "Correct";
+      } else {
+        var c = widget.correctCity;
+        var s = widget.correctState;
+        other = "Correct answer is $c, $s";
+      }
+      footer = Column(
+        children: <Widget>[
+          Text(result),
+          Text(other),
+          Container(
+            height: 30,
+          ),
+          TextButton(
+            style: ButtonStyle(
+              foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+              backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+            ),
+            onPressed: () {
+              setState(() {
+                widget.reset();
+              });
+            },
+            child: Text('Done'),
+          )
+        ]
+      );
+    }
+
     return Column(
       children: <Widget>[
-        Text("Butler"),
         Container(
-          width: 300,
-          child: AutocompleteText(cities),
+          height: 30,
+        ),
+        Text(where_list[widget.idx].name),
+        Container(
+          height: 30,
         ),
         Container(
           width: 300,
-          child: AutocompleteText(states),
-        )
+          child: AutocompleteText(cities, "City", setCityController, ena),
+        ),
+        Container(
+          height: 30,
+        ),
+        Container(
+          width: 300,
+          child: AutocompleteText(states, "State", setStateController, ena),
+        ),
+        Container(
+          height: 30,
+        ),
+        footer
       ]
     );
   }
 }
 
-class MascotGame extends StatelessWidget {
-  const MascotGame({Key? key}) : super(key: key);
+class MascotGame extends StatefulWidget {
+  MascotGame({Key? key, this.idx = 0, this.reset}) : super(key: key);
+
+  int idx;
+  var reset;
+  TextEditingController schoolController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+  String correctSchool = "";
+  String correctName = "";
+  String guessedSchool = "";
+  String guessedName = "";
+
+  @override
+  State<MascotGame> createState() => MascotGameState();
+}
+
+class MascotGameState extends State<MascotGame> {
+  void setController(cntrl) {
+    widget.schoolController = cntrl;
+  }
 
   @override
   Widget build(BuildContext context) {
+    widget.correctSchool = mascot_list[widget.idx].school;
+    widget.correctName = mascot_list[widget.idx].name;
+    List<String> schools = [];
+    mascot_list.forEach((college) {
+      schools.add(college.school);
+    });
+    String n = mascot_list[widget.idx].name;
+    bool ena = (n.length > 1) ? true : false;
+    String lbl = (ena) ? 'Mascot Name' : 'No Mascot Name Known';
+    String result = "";
+    var footer = Column(
+      children: <Widget>[
+        TextButton(
+          style: ButtonStyle(
+            foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+            backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+          ),
+          onPressed: () {
+            setState(() {
+              widget.guessedSchool = widget.schoolController.text;
+              widget.guessedName = widget.nameController.text;
+            });
+          },
+          child: Text('Submit'),
+        ),
+      ]
+    );
+    if(widget.guessedSchool.length > 0) {
+      ena = false;
+      if(widget.guessedSchool == widget.correctSchool && widget.guessedName == widget.correctName) {
+        footer = Column(
+          children: <Widget>[
+            Text("Correct"),
+            Container(
+              height: 30,
+            ),
+            TextButton(
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+              ),
+              onPressed: () {
+                widget.reset();
+              },
+              child: Text('Done'),
+            ),
+          ]
+        );
+      } else {
+        footer = Column(
+          children: <Widget>[
+            Text("Incorrect"),
+            Container(
+              height: 30,
+            ),
+            Text("Correct answer is:"),
+            Text(widget.correctSchool),
+            Text(widget.correctName),
+            Container(
+              height: 30,
+            ),
+            TextButton(
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+              ),
+              onPressed: () {
+                widget.reset();
+              },
+              child: Text('Done'),
+            ),
+          ],
+        );
+      }
+    }
+
     return Column(
       children: <Widget>[
         ClipRRect(
           child: Image.asset(
-            imgs[1],
+            mascot_list[widget.idx].img_path,
             width: 300,
             height: 300,
             fit: BoxFit.fill
           ),
         ),
         Container(
-          width: 300,
-          child: AutocompleteText(names),
-          // child: TextField(
-          //   decoration: InputDecoration(
-          //     border: OutlineInputBorder(),
-          //     labelText: '',
-          //   ),
-          // ),
+          height: 30,
         ),
+        Container(
+          width: 300,
+          child: AutocompleteText(schools, "School", setController, ena),
+        ),
+        Container(
+          height: 30,
+        ),
+        Container(
+          width: 300,
+          child: TextField(
+            enabled: ena,
+            controller: widget.nameController,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: lbl,
+            ),
+          ),
+        ),
+        Container(
+          height: 30,
+        ),
+        footer
       ]
     );
   }
@@ -182,6 +409,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  void resetWindow() {
+    setState(() {
+      widget.window_id = 0;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -237,10 +470,13 @@ class _MyHomePageState extends State<MyHomePage> {
     ],
   );
 
+  math.Random random = math.Random();
   if(widget.window_id == 1) {
-    ctnt = LocationGame();
+    int i = random.nextInt(where_list.length);
+    ctnt = LocationGame(idx: i, reset: resetWindow);
   } else if(widget.window_id == 2) {
-    ctnt = MascotGame();
+    int i = random.nextInt(mascot_list.length);
+    ctnt = MascotGame(idx: i, reset: resetWindow);
   }
 
     return Scaffold(
